d3e7181a64dd5d34d5493754d770919d22095bc6
lucasqmc <lucasleitemm@hotmail.com>
2022-03-23 01:53:29 -0300

    Allow cors on api

--- challenge-backend/src/server.ts
+++ challenge-backend/src/server.ts
@@ -7,8 +7,17 @@ import 'express-async-errors';
 import apiRouter from './routes/api';
 import logger from 'jet-logger';
 import { CustomError } from '@shared/errors';
+import cors from 'cors';
 
 const app = express();
+const allowedOrigins = ['http://localhost:3000'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+// Then pass these options to cors:
+app.use(cors(options));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
